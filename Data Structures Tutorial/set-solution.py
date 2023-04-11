1237e460ef6d23bc7be607db5fcb3c26123b3456
Bryson <117945340+brysonm96@users.noreply.github.com>
2023-04-10 21:55:33 -0600

    Update set-solution.py

--- Data Structures Tutorial/set-solution.py
+++ Data Structures Tutorial/set-solution.py
@@ -1,1 +1,57 @@
-# Solution Code
+#create set
+names = set()
+
+#set length
+SET_MAX = 5
+
+#user prompt
+while True:
+    print("\nCurrent set of names:", names)
+    print("Add a name (1)")
+    print("Remove a name (2)")
+    print("Exit (3)")
+
+    choice = input("Enter the number of your choice: ")
+
+    if choice == "1":
+        name = input("Enter the name to add: ")
+
+        #check to see if name exists
+        if name in names:
+            print("This name exists already")
+            continue
+
+        #check if set is full
+        if len(names) >= SET_MAX:
+            response = input("The set is full, would you like to increase the set size? (y/n): ")
+            if response.lower() == "y":
+                SET_MAX += 1
+                print("Set size increased to", SET_MAX)
+            else:
+                print("Exiting...")
+                break
+
+        #add the name to the set
+        names.add(name)
+        print("Name added")
+
+    elif choice == "2":
+        name = input("Enter a name to remove: ")
+
+        #check to see if name exists
+        if name not in names:
+            print("This name doesn't exist")
+            continue
+
+        #remove the name
+        names.remove(name)
+        print("Name removed")
+
+    elif choice == "3":
+        print("End")
+        break
+
+    else:
+        print("Invalid choice, please try again")
+
+print("Set of names:", names)
